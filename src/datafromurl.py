1f35972ede44b244a68a5be947dcb6274a81b3fd
Denoble <ujay2002@yahoo.com>
2016-06-06 20:50:39 -0230

    synchronizing working dir

--- src/datafromurl.py
+++ src/datafromurl.py
@@ -1,16 +1,19 @@
-filenames=["STJOHNS1988TO1995.txt","STJOHN'SA1996TO2012.txt","STJOHNSWESTCDA1976TO1987.txt"]
+import os
+import urllib
+import requests
 
-for var in filenames:
-    f=open(var,'r')
-    file_count=0;
-    fname= "./Downloaded_files/{}{}.CSV".format(var[:-3],file_count)
-    while 1:
-        line=f.readline()
-        if not line:
-            break
-        if line:
-            line=line[:-1]
-            urlline="'{}'".format(line)
-            request.urlretrieve(urlline,fname)
-            file_count=file_count+1
-    f.close()
+def download_file():
+#takes id and year of a station as argument and download weather file from the url.
+	station_Ids=[50089,5051,27211]
+	years=[2015,2016]
+	for id in station_Ids:
+		for year in years:
+			fname= "./Downloaded_files/{}{}.CSV".format(id,year)
+			url = "http://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID="+str(id)+"&Year="+str(year)+"&Month=8&Day=1&timeframe=2&submit=Download+Data"
+			urllib.request.urlretrieve(url,fname)   
+        
+		
+	return
+	
+download_file()
+
