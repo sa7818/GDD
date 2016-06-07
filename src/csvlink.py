dcc71611c0de029639232f7130eb3fdb3046a9e7
oia133 <oia133@mun.ca>
2016-06-07 20:40:07 -0230

    Robust: Column extraction added

--- src/csvlink.py
+++ src/csvlink.py
@@ -1,6 +1,7 @@
 import os
 import urllib
 import requests
+import pandas as pd
 
 def download_file():
 #Download CSV file from the url provided in the code
@@ -18,7 +19,14 @@ def download_file():
             fname= "../CSV_data/{}/{}_{}.csv".format(city,city,year)
             url = "http://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID="+str(id)+"&Year="+str(year)+"&Month=8&Day=1&timeframe=2&submit=Download+Data"
             urllib.request.urlretrieve(url,fname)   
-    
+            
+#Column extraction below
+            data_frame = pd.read_csv(fname, skiprows=25, sep=",", encoding="ISO-8859-1")
+        #df = pandas.DataFrame("../GDD/CSV_data/"+str(i)+"_"+str(year)+r"_gdd.csv")
+            columns = [0,1,2,3,5,7,9]
+            df = data_frame[columns]
+            new_fname =  "../CSV_data/{}/{}_{}_extract.csv".format(city,city,year)
+            df.to_csv(new_fname)
     return
 
-download_file()
+download_file()   
