284a785c132cec29223dc0e95a0b0297d045905a
Ghassem Alaee Khangha <gak488@mun.ca>
2016-06-08 16:44:09 -0230

    renamed and deleted a file

--- src/download.py
+++ src/download.py
@@ -1,32 +1,40 @@
 import os
 import urllib
 import requests
+import sys
 import pandas as pd
 
-def download_file():
 #Download CSV file from the url provided in the code
-    station_Ids=[50089,5051,27211]
-    years=[2015,2016]
-    city=""
-    for id in station_Ids:
-        if(id==50089):
-            city="st_johns"
-        elif(id==5051):
-            city="toronto"
-        else:
-            city="calgary"
-        for year in years:
-            fname= "../csv_data/{}/{}_{}.csv".format(city,city,year)
-            url = "http://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID="+str(id)+"&Year="+str(year)+"&Month=8&Day=1&timeframe=2&submit=Download+Data"
-            urllib.request.urlretrieve(url,fname)   
-            
-#Column extraction using pandas
-            data_frame = pd.read_csv(fname, skiprows=25, sep=",", encoding="ISO-8859-1")
-        #df = pandas.DataFrame("../GDD/CSV_data/"+str(i)+"_"+str(year)+r"_gdd.csv")
-            columns = [0,1,2,3,5,7,9]
-            df = data_frame[columns]
-            new_fname =  "../csv_data/{}/{}_{}_extract.csv".format(city,city,year)
-            df.to_csv(new_fname)
-    return
 
-download_file()   
+file_name = sys.argv[1]
+#year_value= sys.argv[2]
+city, stationID, year = file_name[:-4].split('_')
+#stationID = file_name
+#year = year_value
+#print(city,stationID, year)
+
+def download(city,stationID,year):
+#	file_name = sys.argv[1]
+#	stationID, year = file_name[:-4].split('-')
+#	print(stationID, year)
+	fname= "../csv_data/{}_{}_{}.csv".format(city,stationID,year)
+	url = "http://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID="+str(stationID)+"&Year="+str(year)+"&Month=8&Day=1&timeframe=2&submit=Download+Data"
+#	urllib.request.urlretrieve(url,fname)
+	urllib.request.urlretrieve(url,fname)
+#Column extraction below
+
+#	data_frame = pandas.read_csv(fname, skiprows=25, sep=",", encoding="ISO-8859-1")
+	data_frame = pd.read_csv(fname, skiprows=25, sep=",", encoding="ISO-8859-1")
+	#df = pandas.DataFrame("../GDD/CSV_data/"+str(i)+"_"+str(year)+r"_gdd.csv")
+	columns = [0,1,2,3,5,7,9]
+	df = data_frame[columns]
+	new_fname =  "../csv_data/{}_{}_{}_extracted.csv".format(city,stationID,year)
+	df.to_csv(new_fname)
+
+	return
+
+download(city,stationID,year)
+#	fname= "../GDD/CSV_data/{}_{}.csv".format(stationID,year)
+#	url = "http://climate.weather.gc.ca/climate_data/bulk_data_e.html?f$
+#	csv_file = urllib.request.urlretrieve(url,fname)
+
