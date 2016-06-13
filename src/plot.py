4abcd0707154431d381a61cf369472894003f844
SharonQSY <shuyue_qi@outlook.com>
2016-06-13 11:27:01 -0230

    Update plot.py

--- src/plot.py
+++ src/plot.py
@@ -8,11 +8,17 @@ from pandas import Series, DataFrame
 import pandas as pd
 import csv
 import sys
-file_name = sys.argv[1]
 
 def read_weather(file_name):
+    """
+    This function is used for read data(Date/Time,Max_Temp,Min_Temp) from csv file
+    args:
+        file_name (csv data file)
+    output:
+        Date/Time,Max_Temp, Min_Temp
+    """
     #Read csv file
-    data=pd.read_csv(file_name, skiprows=25,usecols=(0,6,8),encoding='ISO-8859-1') 
+    data=pd.read_csv(file_name, usecols=(1,5,6),encoding='ISO-8859-1') 
     #replcae E to NAN in csv data file
     data.replace('E', np.nan,inplace=True)
     #replace M to NAN in csv data file
@@ -20,20 +26,42 @@ def read_weather(file_name):
     #Remove NAN in csv data file
     data = data.dropna(how='any')
     Date=data['Date/Time']
-    Max_Temp=data['Max Temp Flag']
-    Min_Temp=data['Min Temp Flag']
+    Max_Temp=data['Max_Temp']
+    Min_Temp=data['Min_Temp']
     return Date,Max_Temp, Min_Temp
 
-def plot_maxmin(csv_file):
+def plot_maxmin(css_file):
+    """
+    This is function for plot Max and Min temp
+    The data read from the above function(read_weather(filename)) 
+    and get the value of Date/Time,Max_Temp, Min_Temp.
+    Based on the Max_Temp, Min_Temp to plot max and min temp
+    argv :
+        filename (csv data file)
+    output:
+        max and min plot, and save the plot into .png format
+    """
     date,max_temp,min_temp=read_weather(css_file)
     #print (max_temp)
     #max_temp=max_temp.astype(float)
     #min_temp=min_temp.astype(float)
     date=pd.to_datetime(date)
-    maxy,=plt.plot_date(date,max_temp,'cx-',label="max temp")
-    mint,=plt.plot_date(date,min_temp,'ro-',label="min temp")
+    maxy,=plt.plot_date(date,max_temp,'ro-',label="max temp")
+    mint,=plt.plot_date(date,min_temp,'cx-',label="min temp")
     plt.legend(handles=[maxy,mint])
     plt.ylabel("temp")
     plt.xlabel("date")
-    plt.title("Min & Max Temp")
-    return plt.savefig("Min & Max Temp.png",format="png")
+    plt.title("Min & Max Temp") 
+    max_plot=plt.savefig(css_file[:-3] + "png",format="png")
+    #return plt.savefig("min_max_temp.png",format="png")
+    return max_plot
+
+try:
+    file_name = sys.argv[1]
+    #file_name = "50089_2015.csv"
+    plot_maxmin(file_name)
+except Exception as e:
+    raise e
+    print (e)
+    
+
